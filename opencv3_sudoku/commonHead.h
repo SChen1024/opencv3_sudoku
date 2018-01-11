a9d647b5994695714a7737314039e6873bb5929f
Hugo <hugochen1024@gmail.com>
2018-01-11 19:08:39 +0800

    opencv3 读取视频

---
+++ opencv3_sudoku/commonHead.h
@@ -0,0 +1,24 @@
+/********************************************************************
+	文件: commonHead.h
+	功能: 常用头文件定义区域
+	备注: 20180111 设立文件
+***********************************************************************/
+#pragma once
+#pragma warning(disable:4996) //全部关掉文件读写函数不加s的错误
+
+#include<iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+
+
+
+
+
+
+
+
--- opencv3_sudoku/main.cpp
+++ opencv3_sudoku/main.cpp
@@ -1,19 +1,51 @@
-#include<iostream>  
-#include <opencv2/core/core.hpp>  
-#include <opencv2/highgui/highgui.hpp>  
+/********************************************************************
+	文件: main.cpp
+	功能: 主函数操作
+	备注: 20180111 陈栓 设立文件
 
+***********************************************************************/
 
-using namespace cv;
+#include "commonHead.h"
 
+/*======================================================================
+	函数: main
+	功能: 主函数操作
+	输入: 无
+	输出: 0
+	备注: 无
 
+======================================================================*/
 int main()
 {
-	// 读入一张图片（游戏原画）  
-	Mat img = imread("../picture/Lena.jpg");
-	// 创建一个名为 "游戏原画"窗口  
-	namedWindow("游戏原画");
-	// 在窗口中显示游戏原画  
-	imshow("游戏原画", img);
-	// 等待6000 ms后窗口自动关闭  
-	waitKey(0);
+	//读入摄像头内容
+	VideoCapture capture(1);
+
+	//灰度图,黑白图,边界图
+	Mat grayFrame, binFrame, edgeFrame;
+
+	//循环显示
+	while (1)
+	{ 
+			Mat srcFrame;
+			capture >> srcFrame;
+
+			//转换为灰度图
+			cvtColor(srcFrame, grayFrame, CV_BGR2GRAY);
+			//转换为黑白图
+			threshold(grayFrame, binFrame, 128, 255, CV_THRESH_OTSU);
+			//模糊化
+			blur(grayFrame, grayFrame, Size(3, 3));
+			//转换为边界图
+			Canny(grayFrame, edgeFrame, 0, 30, 3);
+			
+			imshow("srcFrame", srcFrame);
+			imshow("grayFrame", grayFrame);
+			imshow("binFrame", binFrame);
+			imshow("edgeFrame", edgeFrame);
+
+			waitKey(30);
+	}
+	capture.release();
+	destroyAllWindows();
+	return 0;
 }
